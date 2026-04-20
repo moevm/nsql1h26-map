192459b29bfb06521afb45da1e566c52ba10f7e6
Boris Trunov <144445089+Borl1seTrunov@users.noreply.github.com>
2026-04-20 19:37:17 +0300

    create token-auth-relocate script

---
+++ frontend/src/js/auth.js
@@ -0,0 +1,30 @@
+function getCookie() {
+  return document.cookie.split('; ').reduce((acc, item) => {
+    const [name, value] = item.split('=');
+    acc[name] = value;
+    return acc;
+  }, {})
+}
+
+function isAuthenticated() {
+  const cookies = getCookie();
+  return cookies.token;
+}
+
+export function relocateToLogin() {
+  const isAuth = isAuthenticated();
+  const currentLocation = window.location.pathname;
+
+  if (!isAuth) {
+    if (currentLocation === '/pages/login.html' || currentLocation === '/pages/registration.html') {
+      return;
+    }
+    window.location.href = '/pages/login.html';
+  }
+
+  if (currentLocation === '/pages/login.html' || currentLocation === '/pages/registration.html' || currentLocation === 'index.html') {
+    window.location.href = '/pages/profile.html';
+  }
+
+  return;
+}
