695aeae1b68c370d157e72996d88212d010fdcf9
chelovekkrokant <minorzen2@gmail.com>
2026-04-21 17:51:18 +0300

    feat(db): add email property to db

--- backend/seed.py
+++ backend/seed.py
@@ -81,7 +81,7 @@ async def run_seed(driver):
         await session.run(
             """
             MERGE (u:User {username: 'testuser'})
-            ON CREATE SET u.id = $id, u.password = 'test123', u.avatarUrl = ''
+            ON CREATE SET u.id = $id, u.password = 'test123', u.email = 'testuser@example.com', u.avatarUrl = ''
             """,
             id=str(uuid.uuid4()),
         )
