6e670991b77188faf09af5be37c7646430098232
Boris Trunov <144445089+Borl1seTrunov@users.noreply.github.com>
2026-04-21 21:48:53 +0300

    done: login and register

--- frontend/src/js/pages/loginPage.js
+++ frontend/src/js/pages/loginPage.js
@@ -1,5 +1,43 @@
 import { relocateToLogin } from "../auth";
 
+const login = async (email, password) => {
+  try {
+    const response = await fetch('http://127.0.0.1:10001/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+    
+    if (response.ok && data.token) {
+      document.cookie = `token=${data.token}; Path=/; SameSite=Strict;`;
+      return true;
+    }
+
+    return false;
+  } catch (error) {
+    return false;
+  }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   relocateToLogin();
-})
+
+  const loginBtn = document.querySelector('.sign-in__input--submit');
+  const emailInput = document.querySelector('.sign-in__input--email');
+  const passwordInput = document.querySelector('.sign-in__input--password');
+
+  loginBtn.addEventListener('click', async (evt) => {
+    evt.preventDefault();
+
+    const email = emailInput.value.trim();
+    const password = passwordInput.value;
+
+    const success = await login(email, password);
+    
+    if (success) {
+      relocateToLogin();
+    }
+  });
+});
--- frontend/src/js/pages/registrationPage.js
+++ frontend/src/js/pages/registrationPage.js
@@ -1,5 +1,45 @@
 import { relocateToLogin } from "../auth";
 
+const register = async (nickname, email, password) => {
+  try {
+    const response = await fetch('http://127.0.0.1:10001/api/auth/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ "username": nickname, "email": email, "password": password }),
+    });
+
+    const data = await response.json();
+    
+    if (response.ok && data.token) {
+      document.cookie = `token=${data.token}; Path=/; SameSite=Strict;`;
+      return true;
+    }
+
+    return false;
+  } catch (error) {
+    return false;
+  }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   relocateToLogin();
-})
+
+  const registerBtn = document.querySelector('.sign-up__input--submit');
+  const nicknameInput = document.querySelector('.sign-up__input--nickname');
+  const emailInput = document.querySelector('.sign-up__input--email');
+  const passwordInput = document.querySelector('.sign-up__input--password');
+
+  registerBtn.addEventListener('click', async (evt) => {
+    evt.preventDefault();
+
+    const nickname = nicknameInput.value.trim();
+    const email = emailInput.value.trim();
+    const password = passwordInput.value;
+
+    const success = await register(nickname, email, password);
+    
+    if (success) {
+      relocateToLogin();
+    }
+  });
+});
